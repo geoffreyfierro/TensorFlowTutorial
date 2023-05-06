4f4739bf38eb5636160a4faebcff2a3a176b0923
geoffreyfierro <geoffreyfierro@gmail.com>
2023-05-06 01:32:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -28,5 +28,5 @@ model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=10)
+model.fit(x_train, y_train, epochs=5)
 model.evalute(x_test, y_test, verbose=2)

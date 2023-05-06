b73dc010123cfcae7cfef5cb9f793ba524735b14
geoffreyfierro <geoffreyfierro@gmail.com>
2023-05-06 01:35:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ print(predictions)
 tf.nn.softmax(predictions).numpy()
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-loss_fn(y_train[:1], predictions).numpy()
+#loss_fn(y_train[:1], predictions).numpy()
 
 model.compile(optimizer='adam',
               loss=loss_fn,

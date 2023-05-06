fbe4df6307c6a67362d331e6d3599e3625ee00ee
geoffreyfierro <geoffreyfierro@gmail.com>
2023-05-06 01:23:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 # Geoffrey Fierro
 # TensorFlow Tutorial Practice
 
-import tensorflow
+import tensorflow as tf
+print("TensorFlow version:", tf.__version__)
 
-print("Hello World")
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train/255.0, x_test/255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10),
+])
+
+predictions = model(x_train[:1]).numpy()
+print(predictions)
+
+tf.nn.softmax(predictions).numpy()
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10)
+model.evalute(x_test, y_test, verbose=2)
